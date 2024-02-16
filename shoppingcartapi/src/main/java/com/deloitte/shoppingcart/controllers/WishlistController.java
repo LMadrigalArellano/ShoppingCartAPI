85bc326b7490ebac572ffeb7defe99f4dab3dd0a
LMadrigalArellano@Deloitte.com <LMadrigalArellano@Deloitte.com>
2024-02-16 09:40:53 -0600

    LDMA:
    Added method to add products to the wishlist
    Added method to read wishlist by userId

--- shoppingcartapi/src/main/java/com/deloitte/shoppingcart/controllers/WishlistController.java
+++ shoppingcartapi/src/main/java/com/deloitte/shoppingcart/controllers/WishlistController.java
@@ -3,6 +3,9 @@ package com.deloitte.shoppingcart.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +22,30 @@ public class WishlistController {
 		this.wishlistRepository = wishlistRepository;
 	}
 	
+	/////////////////////////////////---START GET OPERATIONS---/////////////////////////////////
+
 	@GetMapping("/wishlists")
 	public List<Wishlist> getAllWishlists() {
 		return wishlistRepository.findAll();
 	}
+	
+	@GetMapping("/wishlists/user/{userId}")
+	public List<Wishlist> getWishlistByUserId(@PathVariable("userId") int userId){
+		return wishlistRepository.findAllByUserId(userId);
+	}
+	
+	/////////////////////////////////---END GET OPERATIONS---/////////////////////////////////
+	
+	/////////////////////////////////---START POST OPERATIONS---/////////////////////////////////
+	
+	@PostMapping("/wishlists")
+	public void saveProductToWishList(@RequestBody Wishlist wishlist) {
+		wishlistRepository.save(wishlist);
+	}
+	
+	/////////////////////////////////---END POST OPERATIONS---/////////////////////////////////
+
+	
+	
+	
 }
--- shoppingcartapi/src/main/java/com/deloitte/shoppingcart/repos/WishlistRepository.java
+++ shoppingcartapi/src/main/java/com/deloitte/shoppingcart/repos/WishlistRepository.java
@@ -1,9 +1,13 @@
 package com.deloitte.shoppingcart.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.deloitte.shoppingcart.model.Wishlist;
 
 public interface WishlistRepository extends JpaRepository<Wishlist, Integer> {
+	
+	List<Wishlist> findAllByUserId(int userId);
 
 }
